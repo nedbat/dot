ec50853c39a093e76b52a042a873e86f04e44f22
Ned Batchelder <ned@nedbatchelder.com>
2015-09-08 21:32:11 -0400

    readline in .startup.py

--- .startup.py
+++ .startup.py
@@ -13,3 +13,20 @@ def dirx(thing, regex):
     return [ n for n in dir(thing) if re.search(regex, n) ]
 
 pp = pprint.pprint
+
+import atexit
+import os
+import readline
+import rlcompleter
+
+historyPath = os.path.expanduser("~/.pyhistory")
+
+def save_history(historyPath=historyPath):
+    import readline
+    readline.write_history_file(historyPath)
+
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+atexit.register(save_history)
+del atexit, readline, rlcompleter, save_history, historyPath
