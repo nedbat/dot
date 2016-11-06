6eaf85ba884f21b5b48b088605e4f14f2873ce57
Ned Batchelder <ned@nedbatchelder.com>
2016-11-06 09:18:51 -0500

    Conceptually, cleaning up obsolete junk should be a before action

--- after_untar.sh
+++ after_untar.sh
@@ -1,31 +1,5 @@
-# Source this file on unpacking a new dot.tar to remove the local copies of 
+# Commands that will be run after untarring to remove the local copies of 
 # old files, and to put new files into their proper places.
-
-# tabblo days...
-rm -f .path.hulk .path.lnxvcer3 .path.twain
-rm -f .path.g1t0236 .path.g1t0252g .path.g1t0253g .path.g1t0473g .path.g1t0474g
-rm -f .path.marquez .path.thompson .bash_profile.galileo
-rm -f .ssh/ned-davinci.pub .ssh/ned-marquez.pub .ssh/ned-salinger.pub .ssh/ned-twain.pub
-rm -f .ssh/dcs.pub .ssh/eddie.pub
-
-rm -vf .rhosts .cshrc .profile .path.solaris .path.solaris2.8
-rm -vf .emacs .cvspass
-rm -vf .vim/plugin/qfixtoggle.vim
-rm -vf .vim/doc/fuf.* .vim/plugin/fuf.vim
-rm -vrf .vim/autoload/fuf
-
-# Moved to pathogen, remove .vim stuff from the old locations
-rm -vf .vim/doc/NERD_tree.txt .vim/doc/fugitive.txt .vim/doc/qfixtoggle.txt
-rm -vrf .vim/nerdtree_plugin
-rm -vf .vim/plugin/NERD_tree.vim .vim/plugin/fugitive.vim .vim/plugin/minibufexpl.vim
-rm -vf .vim/syntax/nerdtree.vim
-rm -vf .vim/doc/tags
-
-# Moved away from pathogen, no need for bundle any more.
-rm -vrf .vim/bundle
-
-# Old way of handling tar cleanup.
-rm -f .after_untar
 
 # .ssh should be secured.
 chmod 700 .ssh
--- before_untar.sh
+++ before_untar.sh
@@ -1,7 +1,31 @@
-# Commands run before untarring
+# Commands run before untarring.
+
+# tabblo days...
+rm -f .path.hulk .path.lnxvcer3 .path.twain
+rm -f .path.g1t0236 .path.g1t0252g .path.g1t0253g .path.g1t0473g .path.g1t0474g
+rm -f .path.marquez .path.thompson .bash_profile.galileo
+rm -f .ssh/ned-davinci.pub .ssh/ned-marquez.pub .ssh/ned-salinger.pub .ssh/ned-twain.pub
+rm -f .ssh/dcs.pub .ssh/eddie.pub
+
+rm -vf .rhosts .cshrc .profile .path.solaris .path.solaris2.8
+rm -vf .emacs .cvspass
+rm -vf .vim/plugin/qfixtoggle.vim
+rm -vf .vim/doc/fuf.* .vim/plugin/fuf.vim
+rm -vrf .vim/autoload/fuf
+
+# Moved to pathogen, remove .vim stuff from the old locations
+rm -vf .vim/doc/NERD_tree.txt .vim/doc/fugitive.txt .vim/doc/qfixtoggle.txt
+rm -vrf .vim/nerdtree_plugin
+rm -vf .vim/plugin/NERD_tree.vim .vim/plugin/fugitive.vim .vim/plugin/minibufexpl.vim
+rm -vf .vim/syntax/nerdtree.vim
+rm -vf .vim/doc/tags
+
+# Moved away from pathogen, no need for bundle any more.
+rm -vrf .vim/bundle
+
+# Old way of handling tar cleanup.
+rm -f .after_untar
 
 # Plugins are fully provided in the tarball, so delete whatever is there, or
 # we have no way of removing plugins we no longer use.
 rm -rf .vim/plugged
-
-
