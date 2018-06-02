f54aa4f0e6af4ed655a17cf68259f1c6dd0f9c53
Ned Batchelder <ned@nedbatchelder.com>
2018-06-02 15:16:47 -0400

    Don't need double shebangs

--- make_extractor.sh
+++ make_extractor.sh
@@ -4,8 +4,7 @@
 payload=$1
 script=$2
 
-printf "#!/bin/bash\n" > $script
-cat before_untar.sh >> $script
+cat before_untar.sh > $script
 printf "PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
 echo Unpacking
 tail -n+\$PAYLOAD_LINE \$0 | tar -xz
