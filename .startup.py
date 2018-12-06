5a6bdd517258456b9100daf2e71ef13899fbfb04
Ned Batchelder <ned@nedbatchelder.com>
2018-12-06 07:52:25 -0500

    I use collections all the time

--- .startup.py
+++ .startup.py
@@ -7,8 +7,8 @@ try:
     from autoimp import *
     print("(from autoimp import *)")
 except:
-    import datetime, itertools, math, os, pprint, re, sys, time
-    print("(imported datetime, itertools, math, os, pprint, re, sys, time)")
+    import collections, datetime, itertools, math, os, pprint, re, sys, time
+    print("(imported collections, datetime, itertools, math, os, pprint, re, sys, time)")
 
 def dirx(thing, regex):
     return [ n for n in dir(thing) if re.search(regex, n) ]
