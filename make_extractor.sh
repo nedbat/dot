013579c9cbfa4d81c710ab0fc04ba942c90c9d34
Ned Batchelder <ned@nedbatchelder.com>
2019-04-29 08:17:09 -0400

    Ignore tar's stupid 'Ignoring unknown extended header' message

--- make_extractor.sh
+++ make_extractor.sh
@@ -7,7 +7,7 @@ script=$2
 sed -e s/:SCRIPTNAME:/$script/ < before_untar.sh > $script
 printf "PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
 echo Unpacking
-tail -n+\$PAYLOAD_LINE \$0 | base64 --decode | tar -xz
+tail -n+\$PAYLOAD_LINE \$0 | base64 --decode | tar -xz 2>&1 | grep -v 'Ignoring unknown extended header'
 " >> $script
 cat after_untar.sh >> $script
 printf "exit 0
