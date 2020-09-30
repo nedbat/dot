f8fe4db8f944696d9744f147c8dfbcca5ce0ce39
Ned Batchelder <ned@nedbatchelder.com>
2020-09-30 12:51:29 -0400

    gittree can show directories or remotes

--- .rc.sh
+++ .rc.sh
@@ -119,24 +119,33 @@ export TIG_LS_REMOTE="ls-remote-grep -v release-candidate"
 #
 #   $ gittreeif origin/foo 'git log --format="%s" origin/foo ^$(git merge-base origin/master origin/foo)'
 #
-# The directory name is printed before each command.  Use -q to suppress this:
+# The directory name is printed before each command.  Use -q to suppress this,
+# or -r to show the origin remote url instead of the directory name.
 #
 #   $ gittreeif origin/foo -q git status
 #
 gittreeif() {
     local test_branch="$1"
     shift
-    local show=true
+    local show_dir=true show_repo=false
+    if [[ $1 == -r ]]; then
+        # -r means, show the remote url instead of the directory.
+        shift
+        local show_dir=false show_repo=true
+    fi
     if [[ $1 == -q ]]; then
         # -q means, don't echo the separator line with the directory.
         shift
-        local show=false
+        local show_dir=false show_repo=false
     fi
     find . -name .git -type d -prune | while read d; do
         local d=$(dirname "$d")
         git -C "$d" rev-parse --verify -q "$test_branch" >& /dev/null || continue
-        if [[ $show == true ]]; then
+        if [[ $show_dir == true ]]; then
             echo "---- $d ----"
+        fi
+        if [[ $show_repo == true ]]; then
+            echo "----" $(git -C "$d" config --get remote.origin.url) "----"
         fi
         if [[ $# == 1 && $1 == *' '* ]]; then
             (cd "$d" && eval "$1")
