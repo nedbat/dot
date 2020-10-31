d1eee79c9d879852fe8cc1dc52d01b7f5e06086b
Ned Batchelder <ned@nedbatchelder.com>
2020-10-31 19:26:30 -0400

    Make this work for root

--- shex/make_extractor.sh
+++ shex/make_extractor.sh
@@ -7,7 +7,7 @@ script=$2
 sed -e s/:SCRIPTNAME:/$script/ < shex/before_untar.sh > $script
 printf "PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
 echo Unpacking
-tail -n+\$PAYLOAD_LINE \$0 | base64 --decode | tar -xz 2>&1 | grep -v 'Ignoring unknown extended header'
+tail -n+\$PAYLOAD_LINE \$0 | base64 --decode | tar --no-same-owner -xz 2>&1 | grep -v 'Ignoring unknown extended header'
 " >> $script
 cat shex/after_untar.sh >> $script
 printf "exit 0
