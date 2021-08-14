b9a456135e962cf7cb67af03e5975c9ccc97ef76
Ned Batchelder <ned@nedbatchelder.com>
2021-08-14 11:41:32 -0400

    try using McFly for command recall

--- .rc.sh
+++ .rc.sh
@@ -393,6 +393,14 @@ if [[ -f ~/.fzf.$SHELL_TYPE ]]; then
     source ~/.fzf.$SHELL_TYPE
 fi
 
+# https://github.com/cantino/mcfly
+if command -v mcfly >/dev/null; then
+    eval "$(mcfly init zsh)"
+    export MCFLY_FUZZY=true
+    export MCFLY_RESULTS=30
+    export MCFLY_HISTORY_LIMIT=10000
+fi
+
 # Read a local file if it exists.
 
 if [[ -f ~/.rc_local.sh ]]; then
