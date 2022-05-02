e1ea57c9c639c999b0de500690530d1e3b2c9160
Ned Batchelder <ned@nedbatchelder.com>
2022-05-02 05:40:41 -0400

    latest

--- .rc.sh
+++ .rc.sh
@@ -432,7 +432,7 @@ fi
 if [[ -n $PS1 ]]; then
     if command -v mcfly >/dev/null; then
         export MCFLY_RESULTS=30
-        export MCFLY_HISTORY_LIMIT=10000
+        export MCFLY_HISTORY_LIMIT=50000
         eval "$(mcfly init zsh)"
     fi
 fi
