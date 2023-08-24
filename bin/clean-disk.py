38f8b311edac539a4427a37ed860730efe5df1f5
Ned Batchelder <ned@nedbatchelder.com>
2023-08-24 06:35:39 -0400

    more clean-disk.py

--- bin/clean-disk.py
+++ bin/clean-disk.py
@@ -7,7 +7,9 @@ import subprocess
 
 total_saved = 0
 
-def command_output(cmd):
+def command_output(cmd, harden=False):
+    if harden:
+        cmd = f"{cmd} 2>/dev/null || true"
     try:
         return subprocess.check_output(cmd, shell=True).decode("utf-8")
     except Exception as exc:
@@ -18,7 +20,10 @@ def path_for(dirname):
 
 def get_size(dirpath):
     if dirpath.exists():
-        output = command_output(f"du -sk {dirpath}")
+        # -H: Symbolic links on the command line are followed, symbolic links in file hierarchies are not followed.
+        # -s: Display an entry for each specified file.
+        # -k:  Display block counts in 1024-byte (1 kiB) blocks.
+        output = command_output(f"du -Hsk {dirpath}", harden=True)
         size = int(output.split("\t")[0])
     else:
         size = 0
@@ -46,13 +51,14 @@ def rmrf(dirname):
     """Remove completely"""
     shutil.rmtree(dirname, ignore_errors=True)
 
-def cmd(template, doc=None):
+def cmd(template, doc=None, harden=False):
     def doit(dirname):
-        print(command_output(template.format(dirname=dirname)))
+        print(command_output(template.format(dirname=dirname), harden=harden))
     doit.__doc__ = doc or template
     return doit
 
-rm_pyc = cmd("find {dirname} -regex '.*\.py[cow]' -delete", "Delete .pyc etc files")
+rm_pyc = cmd(r"find {dirname} -regex '.*\.py[cow]' -delete", "Delete .pyc etc files")
+rm_tox = cmd(r"find {dirname} -name '.tox' -exec rm -rf {{}} \; -prune", "Delete .tox directories", harden=True)
 
 clean(command_output("brew --cache").strip(), rmrf)
 clean("/usr/local/pyenv/pyenv/cache", rmrf)
@@ -62,7 +68,8 @@ clean("~/Library/Caches/pip", rmrf)
 clean("~/Library/Caches/pipenv", rmrf)
 clean("~/Library/Caches/pip-tools", rmrf)
 clean("~/Library/Caches/yarn", rmrf)
-clean("~/.tox", rmrf)
+clean("~", rm_tox)
+clean("/src", rm_tox)
 clean("/usr/local/virtualenvs", rm_pyc)
 clean("/usr/local/pyenv", rm_pyc)
 clean("/usr/local/pypy", rm_pyc)
