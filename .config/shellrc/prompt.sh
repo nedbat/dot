5aaf210a8f0e533e81f8952d11a1b70980f7441f
Ned Batchelder <ned@anthropic.com>
2024-09-17 11:58:34 -0400

    starship: have to explicitly point it at XDG_CONFIG_HOME

--- .config/shellrc/prompt.sh
+++ .config/shellrc/prompt.sh
@@ -1,6 +1,7 @@
 # Set shell prompt, if we're interactive.
 if [[ -n $PS1 ]]; then
     if command -v starship >/dev/null; then
+        export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship.toml
         plain_prompt() {
             export PS1=$(printf "\n$ ")
         }
