5491bf536f5c8696d3072ead308ad902b46d931f
Ned Batchelder <ned@nedbatchelder.com>
2025-04-03 09:49:54 -0400

    eza

--- .config/shellrc/ls.sh
+++ .config/shellrc/ls.sh
@@ -13,3 +13,9 @@ if [[ $SHELL_TYPE == zsh ]]; then
 fi
 
 alias df='df -k'
+
+
+if command -v eza >/dev/null; then
+    export EZA_CONFIG_DIR=$XDG_CONFIG_HOME/eza
+    alias ll='eza -lF --group-directories-first'
+fi
