264ca2c7a8b6513450c4bb9c14a60dc01e33b317
Ned Batchelder <ned@nedbatchelder.com>
2025-04-05 08:46:29 -0400

    more eza config

--- .config/shellrc/ls.sh
+++ .config/shellrc/ls.sh
@@ -3,19 +3,18 @@
 export LSCOLORS=gxfxDxDxxxDxDxxxxxgxgx
 export CLICOLOR=1
 
-alias ls='ls -F'
-alias l='ls -lFhH'
-alias la='l -A'
-
-if [[ $SHELL_TYPE == zsh ]]; then
-    # https://twitter.com/dailyzshtip/status/1534925273703096322
-    alias lsd='l -d *(/D)'
+if command -v eza &>/dev/null; then
+    export EZA_CONFIG_DIR=$XDG_CONFIG_HOME/eza
+    alias l="eza --long --classify --group --group-directories-first --git"
+    alias la="l -A"
+    alias lsd="l -A --only-dirs"
+else
+    alias l="ls -lFhH"
+    alias la="l -A"
+    if [[ $SHELL_TYPE == zsh ]]; then
+        # https://twitter.com/dailyzshtip/status/1534925273703096322
+        alias lsd='l -d *(/D)'
+    fi
 fi
 
 alias df='df -k'
-
-
-if command -v eza >/dev/null; then
-    export EZA_CONFIG_DIR=$XDG_CONFIG_HOME/eza
-    alias ll='eza -lF --group-directories-first'
-fi
